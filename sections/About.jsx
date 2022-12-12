72954a207d1fcb5e6cef57cb851ea4202663bc66
Dyaa Alyassin <dyaaalyassin@yahoo.com>
2022-12-11 21:16:54 -0500

    Initial commit

---
+++ sections/About.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+      >
+        <TypingText title="| About The Multiverse" textStyles="text-center" />
+
+        <motion.p
+          variants={fadeIn('up', 'tween', 0.2, 1)}
+          className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+        >
+          <span className="font-extrabold text-white">Technoverse</span> is a new
+          thing in the future, where you can enjoy the virtual world by feeling
+          like it's really real, you can feel what you feel in this Technoverse
+          world, because this is really the{' '}
+          <span className="font-extrabold text-white">
+            madness of the Technoverse
+          </span>{' '}
+          of today, using only{' '}
+          <span className="font-extrabold text-white">VR</span> devices you can
+          easily explore the Technoverse world you want, turn your dreams into
+          reality. Let's{' '}
+          <span className="font-extrabold text-white">explore</span> the madness
+          of the Technoverse by scrolling down
+        </motion.p>
+
+      </motion.div>
+    </div>
+  </section>
+);
+
+export default About;
